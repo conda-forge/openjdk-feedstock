b11f1572673104b33b719cb2cedb42e49602df2a
Patrick Sodré <sodre@sodre.co>
2017-01-28 03:12:39 +0000

    Add JNI test for os-x

--- recipe/test-jni.sh
+++ recipe/test-jni.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 set -e
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
+JAVA_LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server
+if [ $os == 'darwin' ]; then
+	JAVA_LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+fi
+
 gcc \
 	-I$JAVA_HOME/include 			\
-	-I$JAVA_HOME/include/linux 		\
-	-L$JAVA_HOME/jre/lib/amd64/server 	\
+	-I$JAVA_HOME/include/$os		\
+	-L$JAVA_LD_LIBRARY_PATH			\
 	-ljvm					\
 	-o vmtest				\
 	test-jni/vmtest.c 
 	
-vmtest
+if [ $os == 'darwin' ]; then
+	DYLD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH ./vmtest
+elif [ $os == 'linux']; then
+	LD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH ./vmtest
+fi
