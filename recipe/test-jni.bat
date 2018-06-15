ddf9c987482f20cf5f6deb94630fb257e6111b33
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-06-15 12:10:31 -0400

    Test JNI on Windows
    Basically copies over the test from Unix and tries to implement it for
    Windows.
    
    Add missing include path to the Windows test
    
    Fix up compiler options

---
+++ recipe/test-jni.bat
@@ -0,0 +1,12 @@
+cl ^
+	test-jni/vmtest.c                       ^
+	/I"%JAVA_HOME%\include"			^
+	/I"%JAVA_HOME%\include\win32"		^
+	/link					^
+	/LIBPATH:"%JAVA_HOME%\lib"		^
+	/LIBPATH:"%JAVA_HOME%\jre\bin\server"	^
+	jvm.lib
+if errorlevel 1 exit 1
+
+.\vmtest.exe
+if errorlevel 1 exit 1
