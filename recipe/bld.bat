f042024779ef18446d0bae939fc90785df47c7e6
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-06-15 12:08:31 -0400

    Tidy up Windows build script
    Include `errorlevel` checks and lower case commands as Windows is not
    case sensitive anyways.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,25 @@
 setlocal EnableDelayedExpansion
 
-MOVE bin\* %LIBRARY_BIN%
-MOVE include\* %LIBRARY_INC%
-MOVE jre %LIBRARY_PREFIX%\jre
-MOVE lib\* %LIBRARY_LIB%
-MOVE src.zip %LIBRARY_PREFIX%\jre\src.zip
+move bin\* %LIBRARY_BIN%
+if errorlevel 1 exit 1
+
+move include\* %LIBRARY_INC%
+if errorlevel 1 exit 1
+
+move jre %LIBRARY_PREFIX%\jre
+if errorlevel 1 exit 1
+
+move lib\* %LIBRARY_LIB%
+if errorlevel 1 exit 1
+
+move src.zip %LIBRARY_PREFIX%\jre\src.zip
+if errorlevel 1 exit 1
 
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
 :: This will allow them to be run on environment activation.
 FOR %%F IN (activate deactivate) DO (
-    IF NOT EXIST %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
-    COPY %RECIPE_DIR%\scripts\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    if not exist %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
+    if errorlevel 1 exit 1
+    copy %RECIPE_DIR%\scripts\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    if errorlevel 1 exit 1
 )
