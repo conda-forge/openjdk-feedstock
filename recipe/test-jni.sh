e93f1bb77307bfd343a596b63fbf046929116ac2
conda-forge <conda@conda-forge.github.io>
2018-11-22 06:34:02 +0000

    Use compiler tag during test section

--- recipe/test-jni.sh
+++ recipe/test-jni.sh
@@ -7,7 +7,7 @@ if [ ! -d $JAVA_LD_LIBRARY_PATH ]; then
 fi
 
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
-gcc -I$JAVA_HOME/include \
+${CC} -I$JAVA_HOME/include \
 	-I$JAVA_HOME/include/$os \
 	-Wl,-rpath,$JAVA_LD_LIBRARY_PATH \
 	-L$JAVA_LD_LIBRARY_PATH \
