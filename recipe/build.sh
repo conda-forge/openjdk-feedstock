e63e0ebca6c2489390633107b2cbe46040c40b7a
Isuru Fernando <isuruf@gmail.com>
2019-08-11 04:37:03 +0000

    build from source

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,43 +1,92 @@
 #!/bin/bash -euo
 
-chmod +x bin/*
-mkdir -p $PREFIX/bin
-mv bin/* $PREFIX/bin/
-ls -la $PREFIX/bin
+function jdk_install
+{
+  chmod +x bin/*
+  mkdir -p $INSTALL_DIR/bin
+  mv bin/* $INSTALL_DIR/bin/
+  ls -la $INSTALL_DIR/bin
 
-mkdir -p $PREFIX/include
-mv include/* $PREFIX/include
-if [ -e ./lib/jspawnhelper ]; then
+  mkdir -p $INSTALL_DIR/include
+  mv include/* $INSTALL_DIR/include
+  if [ -e ./lib/jspawnhelper ]; then
     chmod +x ./lib/jspawnhelper
+  fi
+
+  mkdir -p $INSTALL_DIR/lib
+  mv lib/* $INSTALL_DIR/lib
+
+  #mv DISCLAIMER $INSTALL_DIR/DISCLAIMER
+
+  mkdir -p $INSTALL_DIR/conf
+  mv conf/* $INSTALL_DIR/conf
+
+  mkdir -p $INSTALL_DIR/jmods
+  mv jmods/* $INSTALL_DIR/jmods
+
+  mkdir -p $INSTALL_DIR/legal
+  mv legal/* $INSTALL_DIR/legal
+
+  mkdir -p $INSTALL_DIR/man/man1
+  mv man/man1/* $INSTALL_DIR/man/man1
+  rm -rf man/man1
+  mv man/* $INSTALL_DIR/man
+}
+
+function source_build
+{
+  cd src
+  export CPATH=$PREFIX/include
+  export LIBRARY_PATH=$PREFIX/lib
+  export LIBS="-liconv"
+
+  if [[ "$target_platform" == linux* ]]; then
+    rm $PREFIX/include/iconv.h
+  fi
+
+  export -n CFLAGS
+  export -n CXXFLAGS
+  export -n LDFLAGS
+
+  chmod +x configure
+  ./configure \
+    --prefix=$PREFIX \
+    --with-extra-cflags="$CFLAGS" \
+    --with-extra-cxxflags="$CFLAGS" \
+    --with-extra-ldflags="$LDFLAGS" \
+    --with-x=$PREFIX \
+    --with-cups=$PREFIX \
+    --with-freetype=system \
+    --with-fontconfig=$PREFIX \
+    --with-giflib=system \
+    --with-libpng=system \
+    --with-zlib=system \
+    --with-libjpeg=system \
+    --with-stdc++lib=dynamic \
+    --with-boot-jdk=$SRC_DIR/bootjdk
+
+  make JOBS=$CPU_COUNT
+  make images
+}
+
+if [[ "$target_platform" == linux* ]]; then 
+  export INSTALL_DIR=$SRC_DIR/bootjdk/
+  jdk_install
+  source_build
+  cd build/*/images/jdk
 fi
 
-if [[ `uname` == "Linux" ]]
-then
-    mv lib/jli/*.so lib/
+export INSTALL_DIR=$PREFIX
+jdk_install
 
-    # Include dejavu fonts to allow java to work even on minimal cloud
-    # images where these fonts are missing (thanks to @chapmanb)
-    mkdir -p lib/fonts
-    mv ./fonts/ttf/* ./lib/fonts/
-    rm -rf ./fonts
+if [[ "$target_platform" == linux* ]]; then
+  mv lib/jli/*.so $INSTALL_DIR/lib/
+  # Include dejavu fonts to allow java to work even on minimal cloud
+  # images where these fonts are missing (thanks to @chapmanb)
+  mkdir -p lib/fonts
+  mv $SRC_DIR/fonts/ttf/* ./lib/fonts/
 fi
 
-mkdir -p $PREFIX/lib
-mv lib/* $PREFIX/lib
-
-mv DISCLAIMER $PREFIX/DISCLAIMER
-
-mkdir -p $PREFIX/conf
-mv conf/* $PREFIX/conf
-
-mkdir -p $PREFIX/jmods
-mv jmods/* $PREFIX/jmods
-
-mkdir -p $PREFIX/legal
-mv legal/* $PREFIX/legal
-
-mkdir -p $PREFIX/man
-mv man/* $PREFIX/man
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
