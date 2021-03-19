5f1d24e96d09e657476b060f8c6ce01b8830aaa3
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-19 14:35:12 +0000

    Cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -52,32 +52,93 @@ function source_build
 {
   cd src
 
+  chmod +x configure
+
   if [[ "$target_platform" == linux* ]]; then
     rm $PREFIX/include/iconv.h
   fi
 
+
+  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
+    (
+      if [[ "$build_platform" == linux* ]]; then
+        rm $BUILD_PREFIX/include/iconv.h
+      fi
+
+      export CPATH=$BUILD_PREFIX/include
+      export LIBRARY_PATH=$BUILD_PREFIX/lib
+      export CC=${CC_FOR_BUILD}
+      export CXX=${CXX_FOR_BUILD}
+      export CPP=${CXX_FOR_BUILD//+/p}
+      export NM=$($CC_FOR_BUILD -print-prog-name=nm)
+      export AR=$($CC_FOR_BUILD -print-prog-name=ar)
+      export OBJCOPY=$($CC_FOR_BUILD -print-prog-name=objcopy)
+      export STRIP=$($CC_FOR_BUILD -print-prog-name=strip)
+      export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+      # CFLAGS and CXXFLAGS are intentionally empty
+      export -n CFLAGS
+      export -n CXXFLAGS
+      unset CPPFLAGS
+
+      CFLAGS=$(echo $CFLAGS | sed 's/-mcpu=[a-z0-9]*//g' | sed 's/-mtune=[a-z0-9]*//g' | sed 's/-march=[a-z0-9]*//g')
+      CXXFLAGS=$(echo $CXXFLAGS | sed 's/-mcpu=[a-z0-9]*//g' | sed 's/-mtune=[a-z0-9]*//g' | sed 's/-march=[a-z0-9]*//g')
+
+      ulimit -c unlimited
+      mkdir build-build
+        pushd build-build
+        ../configure \
+          --prefix=${BUILD_PREFIX} \
+          --build=${BUILD} \
+          --host=${BUILD} \
+          --target=${BUILD} \
+          --with-extra-cflags="${CFLAGS//$PREFIX/$BUILD_PREFIX}" \
+          --with-extra-cxxflags="${CXXFLAGS//$PREFIX/$BUILD_PREFIX} -fpermissive" \
+          --with-extra-ldflags="${LDFLAGS//$PREFIX/$BUILD_PREFIX}" \
+          --with-stdc++lib=dynamic \
+          --disable-warnings-as-errors \
+          --with-x=${BUILD_PREFIX} \
+          --with-cups=${BUILD_PREFIX} \
+          --with-freetype=system \
+          --with-giflib=system \
+          --with-libpng=system \
+          --with-zlib=system \
+          --with-libjpeg=system \
+          --with-lcms=system \
+          --with-fontconfig=${BUILD_PREFIX} \
+          --with-boot-jdk=$SRC_DIR/bootjdk
+        make JOBS=$CPU_COUNT images
+      popd
+    )
+  fi
+
+
   export CPATH=$PREFIX/include
   export LIBRARY_PATH=$PREFIX/lib
-
-  export -n CFLAGS
-  export -n CXXFLAGS
-  export -n LDFLAGS
-
   export BUILD_CC=${CC_FOR_BUILD}
   export BUILD_CXX=${CXX_FOR_BUILD}
+  export BUILD_CPP=${CXX_FOR_BUILD//+/p}
   export BUILD_NM=$($CC_FOR_BUILD -print-prog-name=nm)
   export BUILD_AR=$($CC_FOR_BUILD -print-prog-name=ar)
   export BUILD_OBJCOPY=$($CC_FOR_BUILD -print-prog-name=objcopy)
   export BUILD_STRIP=$($CC_FOR_BUILD -print-prog-name=strip)
 
-  chmod +x configure
+  export -n CFLAGS
+  export -n CXXFLAGS
+  export -n LDFLAGS
+
+  CONFIGURE_ARGS=""
+  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
+    CONFIGURE_ARGS="--with-build-jdk=$SRC_DIR/src/build-build/images/jdk"
+  fi
+
   ./configure \
     --prefix=$PREFIX \
     --build=${BUILD} \
     --host=${HOST} \
     --target=${HOST} \
     --with-extra-cflags="$CFLAGS" \
-    --with-extra-cxxflags="$CFLAGS" \
+    --with-extra-cxxflags="$CXXFLAGS -fpermissive" \
     --with-extra-ldflags="$LDFLAGS" \
     --with-x=$PREFIX \
     --with-cups=$PREFIX \
@@ -90,10 +151,11 @@ function source_build
     --with-lcms=system \
     --with-stdc++lib=dynamic \
     --disable-warnings-as-errors \
-    --with-boot-jdk=$SRC_DIR/bootjdk
+    --with-boot-jdk=$SRC_DIR/bootjdk \
+    ${CONFIGURE_ARGS}
 
   make JOBS=$CPU_COUNT
-  make images
+  make JOBS=$CPU_COUNT images
 }
 
 if [[ "$target_platform" == linux* ]]; then 
