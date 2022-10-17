96290f57a35d96c184daee80e3e801fbadf3d68e
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2022-10-17 22:01:44 +0200

    Fix windows installation

--- recipe/scripts/activate.bat
+++ recipe/scripts/activate.bat
@@ -1,3 +1,4 @@
 @echo off
 set "JAVA_HOME_CONDA_BACKUP=%JAVA_HOME%"
 set "JAVA_HOME=%CONDA_PREFIX%\Library\lib\jvm"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
--- recipe/scripts/deactivate.bat
+++ recipe/scripts/deactivate.bat
@@ -1,3 +1,7 @@
 @echo off
+setlocal EnableDelayedExpansion
+set "NEW_PATH=!PATH:%JAVA_HOME%\bin;=!"
+endlocal & set "PATH=%NEW_PATH%"
+set "NEW_PATH="
 set "JAVA_HOME=%JAVA_HOME_CONDA_BACKUP%"
 set "JAVA_HOME_CONDA_BACKUP="
