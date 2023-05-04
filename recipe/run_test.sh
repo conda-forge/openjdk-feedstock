4426513f09b901ccde64e09d074558c364172d98
Matthias Bernt <m.bernt@ufz.de>
2023-05-04 16:52:03 +0200

    add verbose flag to javac

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -9,7 +9,7 @@ if [ "${JAVA_HOME}" != "${PREFIX}/lib/jvm" ] && [ "${JAVA_HOME}" != "${PREFIX}/L
 fi
 
 pushd test-nio
-  javac TestFilePaths.java
+  javac -verbose TestFilePaths.java || cat *.log
   jar cfm TestFilePaths.jar manifest.mf TestFilePaths.class
   java -jar TestFilePaths.jar TestFilePaths.java
 popd
