f97ab6526f5fed1d82f44283d1f367bb19048557
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-09-08 16:56:11 +0200

    Fixes #87. Ensure deactivate.sh works if activate.sh is not run
    It can happen that the deactivate script is run when the activate
    script has not been run. The deactivate script should therefore
    be robust in verifying all relevant variables are set.

--- recipe/scripts/deactivate.sh
+++ recipe/scripts/deactivate.sh
@@ -1,11 +1,22 @@
-export JAVA_HOME=$JAVA_HOME_CONDA_BACKUP
-unset JAVA_HOME_CONDA_BACKUP
-if [ -z $JAVA_HOME ]; then
-    unset JAVA_HOME
+# First check whether the backup variables are set.
+# The backed up variables are allowed to be set empty.
+# Then restore the backup, and unset the backup.
+
+# Note that the check whether the backup is set, is essential.
+# There are situations where conda executes the deactivate
+# script without having called the activate script. Without
+# this check, the deactivate script would unset JAVA_HOME in
+# those situations.
+
+# One such situation occurs when deactivating the environment
+# after installing openjdk for the first time.
+
+if [ "${JAVA_HOME_CONDA_BACKUP+x}" ] ; then
+  export JAVA_HOME=$JAVA_HOME_CONDA_BACKUP
+  unset JAVA_HOME_CONDA_BACKUP
 fi
 
-export JAVA_LD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH_BACKUP
-unset JAVA_LD_LIBRARY_PATH_BACKUP
-if [ -z $JAVA_LD_LIBRARY_PATH ]; then
-    unset JAVA_LD_LIBRARY_PATH
+if [ "${JAVA_LD_LIBRARY_PATH_BACKUP+x}" ] ; then
+  export JAVA_LD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH_BACKUP
+  unset JAVA_LD_LIBRARY_PATH_BACKUP
 fi
