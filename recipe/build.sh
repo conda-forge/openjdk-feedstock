734f13ce6853101407970368dd847f6964bd5a6f
H. Vetinari <h.vetinari@gmx.com>
2024-06-30 17:43:41 +1100

    build osx from source

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-
 set -exuo pipefail
 
 JVM_BUILD_LOG_LEVEL=warn
-
-# Remove code signatures from osx-64 binaries as they will be invalidated in the later process.
-# TODO: Fix https://github.com/thefloweringash/sigtool to add --remove-signature support
-if [[ "${target_platform}" == "osx-64" ]]; then
-  for b in `ls bin`; do
-    /usr/bin/codesign --remove-signature bin/$b
-  done
-  for b in `ls lib/*.dylib lib/*.dylib.* lib/**/*.dylib`; do
-    /usr/bin/codesign --remove-signature $b
-  done
-  /usr/bin/codesign --remove-signature lib/jspawnhelper
-fi
 
 echo "--------------------------------------------------"
 env | sort
@@ -68,16 +55,12 @@ function source_build
 
   chmod +x configure
 
-  if [[ "$target_platform" == linux* ]]; then
-    rm $PREFIX/include/iconv.h
-  fi
+  rm $PREFIX/include/iconv.h
 
 
   if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
     (
-      if [[ "$build_platform" == linux* ]]; then
-        rm $BUILD_PREFIX/include/iconv.h
-      fi
+      rm $BUILD_PREFIX/include/iconv.h
 
       export CPATH=$BUILD_PREFIX/include
       export LIBRARY_PATH=$BUILD_PREFIX/lib
@@ -208,12 +191,10 @@ function source_build
   make JOBS=$CPU_COUNT images $_TOOLCHAIN_ARGS || printerror
 }
 
-if [[ "$target_platform" == linux* ]]; then
-  export INSTALL_DIR=$SRC_DIR/bootjdk/
-  jdk_install
-  source_build
-  cd build/*/images/jdk
-fi
+export INSTALL_DIR=$SRC_DIR/bootjdk/
+jdk_install
+source_build
+cd build/*/images/jdk
 
 export INSTALL_DIR=$PREFIX/lib/jvm
 jdk_install
@@ -229,15 +210,12 @@ for i in $(find $INSTALL_DIR/man -type f -printf '%P\n'); do
     ln -s -r -f "$INSTALL_DIR/man/$i" "$PREFIX/man/$i"
 done
 
-if [[ "$target_platform" == linux* ]]; then
-  # This is not present on AdoptOpenJDK>=17 and appears to have been replaced with $INSTALL_DIR/libjli.so
-  # mv $INSTALL_DIR/lib/jli/*.so $INSTALL_DIR/lib/
-    
-  # Include dejavu fonts to allow java to work even on minimal cloud
-  # images where these fonts are missing (thanks to @chapmanb)
-  mkdir -p $INSTALL_DIR/lib/fonts
-  mv $SRC_DIR/fonts/ttf/* $INSTALL_DIR/lib/fonts/
-fi
+# Include dejavu fonts to allow java to work even on minimal cloud
+# images where these fonts are missing (thanks to @chapmanb)
+mkdir -p $INSTALL_DIR/lib/fonts
+
+mv $SRC_DIR/fonts/ttf/* $INSTALL_DIR/lib/fonts/
+
 find $PREFIX -name "*.debuginfo" -exec rm -rf {} \;
 
 
