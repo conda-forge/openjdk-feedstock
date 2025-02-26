2e334ab4c0438daf2f1f7cb2f6aaf65a12d8fa75
hinerm <hinerm@gmail.com>
2025-02-26 11:39:11 -0600

    Add defensive check to [de]activate batch scripts
    The deactivate script should now return JAVA_HOME to its previous state
    (including being unset) if and only if the activate script was run
    first.

--- recipe/scripts/deactivate.bat
+++ recipe/scripts/deactivate.bat
@@ -1,2 +1,15 @@
-set "JAVA_HOME=%JAVA_HOME_CONDA_BACKUP%"
-set "JAVA_HOME_CONDA_BACKUP="
+@echo off
+rem First check whether backup variable(s) is/are set.
+rem Variables are allowed to be set empty (indicates unset).
+rem If a backup variable is set, restore it and then unset the backup.
+
+if defined JAVA_HOME_CONDA_BACKUP (
+    rem A literal value of "ENV_VAR_UNSET" indicates the variable was originally
+    rem unset before running the corresponding activate script.
+    if "%JAVA_HOME_CONDA_BACKUP%"=="ENV_VAR_UNSET" (
+        set "JAVA_HOME="
+    ) else (
+        set "JAVA_HOME=%JAVA_HOME_CONDA_BACKUP%"
+    )
+    set "JAVA_HOME_CONDA_BACKUP="
+)
